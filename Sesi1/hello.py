6ae3b7aa3913ed9cf9792f924181a92b391335a2
agung wicaksono <wicaksonoagung05@gmail.com>
2021-12-17 08:56:30 +0700

    first commit

---
+++ Sesi1/hello.py
@@ -0,0 +1,121 @@
+# # Integers
+# print(123123123 + 1)
+# print(10)
+# print(type(10))
+
+# # Floating-Point Numbers
+# print(4.2)
+# print(type(4.2))
+# print(4.)
+# print(.2)
+# print(.4e7)
+# print(4.2e-4)
+
+# # Strings
+# print("Hactiv8")
+# print(type("Hactiv8"))
+# print("This string contains a single quote (') caharacter.")
+# print('This string contain a double quote(") character.')
+
+# # Boolean
+# print(type(True))
+# print(type(False))
+# print (100>200)
+# print (100==200)
+# print (100<200)
+
+# # Variable Assignment
+# n = 300
+# print(n)
+
+# a=b=c=300
+# print(a,b,c)
+
+# # Variable Names
+# name = "Hactive8"
+# Age = 54
+# has_laptops = True
+# # print(name, Age, has_laptops)
+
+# # Camel Case, Example: numberOfCollegeGraduates
+# # Pascal Case, Example: NumberOfCollegeGraduates
+# # Snake Case, Example: number_of_college_graduates
+
+# # Operators and Expressions
+# a = 10
+# b = 20
+# print(a+b)
+# print(a+b-5)
+
+# # Aritmetic operators
+# a = 4
+# b = 3
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a % b)
+# print(a**b)
+
+# # Comparasion Operators
+# a=10
+# b=20
+# print(a==b)
+# print(a!=b)
+# print(a<=b)
+# print(a>=b)
+
+# # String Manipulation
+# s = 'foo'
+# t = 'bar'
+# # + and * operators
+# print(s+t)
+# print(s*4)
+# # Case Conversion
+# print(s.capitalize())
+# print(s.lower())
+# print(s.swapcase())
+
+# # Python list
+# a = ['foo', 'bar', 'qux']
+# print(a)
+
+# # modifying list value
+# a = ['foo', 'bar', 'baz', 'quux', 'corge']
+# a[2] = 10
+# a[-1] = 20
+# print(a)
+# print(a[1:4])
+# a[1:4] = [1.1, 2.2, 3.3, 4.4, 5.5]
+# print(a)
+
+# # python tuples
+# t = ('foo', 'bar', 'baz', 'quux', 'corge')
+# print(t)
+# print(t[0])
+# print(t[-1])
+# # packing and unpacking
+# (s1,s2,s3,s4)=('foo', 'bar', 'baz', 'qux')
+# print(s1)
+
+# # pyton dictionary
+# MLB_team = {
+#     'Colorado': 'Rockies',
+#     'Bostom': 'Red Sox',
+#     'Minnesota': 'Twins'
+# }
+# print(MLB_team['Minnesota'])
+# # Adding an entry to an existing dictionary
+# MLB_team['Kansas City'] = 'Royals'
+# print(MLB_team)
+
+# building a dictionary incrementally
+person = {}
+person['name'] = 'hack'
+person['lname'] = '8'
+person['age'] = 51
+person['spouse'] = 'Edna'
+person['children'] = ['Ralph', 'Betty', 'Joey']
+person['pets'] = {'dog': 'FIdo', 'cat': 'Sox'}
+print(person)
+print(person['children'])
